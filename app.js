17147572a002d32b73cf156211f62b632b661112
Kevin Potts <34223013+skydivekevin@users.noreply.github.com>
2018-08-31 11:38:17 -0600

    consistent semi-colons

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const csvPath = path.join(__dirname, 'db', 'dataset.csv')
 const options = { header: true, skipEmptyLines: true, dynamicTyping: true }
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
   next();
 });
 
